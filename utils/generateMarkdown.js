384a93ab00e58414c4444edf31a3b8eca8379bad
Sophie <sophiedodsworth@hotmail.com>
2023-03-11 15:00:30 +0000

    began adding to js file that generates markdown with table of contents

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,6 +2,44 @@
 function generateMarkdown(data) {
   return `# ${data.title}
 
+  ##Description
+
+  ${data.description}
+
+  ## Table of Contents 
+
+  * [License](#license)
+  * [Installation](#installation)
+  * [Tests](#tests)
+  * [Usage](#usage)
+  * [Contrubuting](#contributing)
+  * [Questions](#questions)
+  
+  ## License
+
+  ${data.license}
+
+  ## Installation
+
+  ${data.install}
+
+  ## Tests
+
+  ${data.tests}
+
+  ## Usage
+
+  ${data.usage}
+
+  ## Contributing
+
+  ${data.contributing}
+
+  ## Questions
+
+  ${data.profile}
+  ${data.email}
+
 `;
 }
 
